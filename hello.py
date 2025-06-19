a9c38ade28ce9ca87acffa912034c2622ed6da51
zerixmax <zerix.m@gmail.com>
2025-06-19 13:45:18 +0200

    Prvi commit - dodane početne datoteke

---
+++ hello.py
@@ -0,0 +1,9 @@
+#  hello.py moja prva python datotrka
+print("Allo Svijete!")
+print("Dobrodošli u Olea Digitalis  razvoj")
+
+# Varijable za projekt masline 
+naziv_masline = "Orgula"
+starost = 75
+je_zdrava = True
+print(f"Maslina {naziv_masline} je stara {starost} godina")
